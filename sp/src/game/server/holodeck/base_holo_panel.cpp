b5867831878ca200b7691293ea44d6b6f29bb6de
lzadrozny <turbo_sgtls@hotmail.com>
2014-07-30 11:48:16 +1000

    Panels can now specify a model to glow.

--- sp/src/game/server/holodeck/base_holo_panel.cpp
+++ sp/src/game/server/holodeck/base_holo_panel.cpp
@@ -10,6 +10,16 @@
 #include "cbase.h"
 #include "base_holo_panel.h"
 
+BEGIN_DATADESC( CBaseHoloPanel )
+
+	// Save state.
+	DEFINE_FIELD( _glowTarget, FIELD_EHANDLE ),
+
+	// Hammer attributes.
+	DEFINE_KEYFIELD( _glowTargetName, FIELD_STRING, "glowtarget" ),
+
+END_DATADESC()
+
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 void CBaseHoloPanel::Spawn()
@@ -18,6 +28,16 @@ void CBaseHoloPanel::Spawn()
 
 	// This entity can only be triggered once every second.
 	m_flWait = 1.0f;
+
+	InitEntityGlow();
+}
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+void CBaseHoloPanel::UpdateOnRemove()
+{
+	RemoveEntityGlow();
+	BaseClass::UpdateOnRemove();
 }
 
 //-----------------------------------------------------------------------------
@@ -40,4 +60,46 @@ bool CBaseHoloPanel::PassesTriggerFilters( CBaseEntity *pOther )
 
 	// Only allow the hand to interact with Holodeck triggers.
 	return pHand->entindex() == pOther->entindex();
+}
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+void CBaseHoloPanel::InitEntityGlow()
+{
+	_glowTarget.Set( NULL );
+	if( _glowTargetName != NULL_STRING )
+	{
+		// Find the entity that we must make glow.
+		_glowTarget = gEntList.FindEntityByName( NULL, STRING( _glowTargetName ) );
+
+		if( !_glowTarget )
+		{
+			// The entity name doesn't exist.
+			Warning( "HOLODECK: Gesture panel references invalid glow entity \"%s\"\n", STRING( _glowTargetName ) );
+		}
+		else
+		{
+			// Ensure the target entity is a model based entity.
+			CBaseAnimating *target = dynamic_cast<CBaseAnimating *>( _glowTarget.Get() );
+			if( !target )
+			{
+				Warning( "HOLODECK: Gesture panel references non studio model glow entity \"%s\"\n", STRING( _glowTargetName ) );
+			}
+			else
+			{
+				target->AddGlowEffect();
+			}
+		}
+	}
+}
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+void CBaseHoloPanel::RemoveEntityGlow()
+{
+	CBaseAnimating *target = dynamic_cast<CBaseAnimating *>( _glowTarget.Get() );
+	if( target )
+	{
+		target->RemoveGlowEffect();
+	}
 }
--- sp/src/game/server/holodeck/base_holo_panel.h
+++ sp/src/game/server/holodeck/base_holo_panel.h
@@ -18,12 +18,24 @@ class CBaseHoloPanel : public CTriggerMultiple
 {
 public:
 	DECLARE_CLASS( CBaseHoloPanel, CTriggerMultiple );
+	DECLARE_DATADESC();
 
 	// CBaseEntity overrides.
 	virtual void	Spawn();
+	virtual void	UpdateOnRemove();
 
 	// CTriggerMultiple overrides.
 	virtual bool	PassesTriggerFilters( CBaseEntity *pOther );
+
+private:
+	void			InitEntityGlow();
+	void			RemoveEntityGlow();
+
+	// Save state.
+	EHANDLE			_glowTarget;
+
+	// Hammer attributes.
+	string_t		_glowTargetName;
 };
 
 #endif // __BASE_HOLO_PANEL_H__
