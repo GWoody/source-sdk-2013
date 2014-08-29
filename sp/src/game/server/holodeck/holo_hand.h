d37922b6558b432f66ec374bff13023a7b59e847
lucasz93 <turbo_sgtls@hotmail.com>
2014-08-29 12:41:50 +1000

    Dun goofed

---
+++ sp/src/game/server/holodeck/holo_hand.h
@@ -0,0 +1,23 @@
+/*
+===============================================================================
+
+	holo_hand.cpp
+		Implements animation for the hand entity.
+
+===============================================================================
+*/
+
+#ifndef __HOLO_HAND_H__
+#define __HOLO_HAND_H__
+
+#include "holo_base_hand.h"
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+class CHoloHand : public CBaseHoloHand
+{
+public:
+	DECLARE_CLASS( CHoloHand, CBaseHoloHand );
+};
+
+#endif // __HOLO_HAND_H__
